dc47b171d94d1289cbb52820f1691f890c5f7816
Zeeshan Islam <159948421+Rohaan001@users.noreply.github.com>
2024-10-04 09:26:20 +0500

    add

--- app.js
+++ app.js
@@ -1,40 +1,100 @@
-// task 1
-// let sentence = "hello my name is zeeshan Islam";
-// let words = sentence.split(" ");
-// let capitalizedWords = [];
+//JS Assignment 10
 
-// for (let i = 0; i < words.length; i++) {
-//     let word = words[i];
-//     let capitalizedWord = word.slice(0,1).toUpperCase() + word.slice(1);
-//     capitalizedWords.push(capitalizedWord);
+//Task 01
+
+// var first_name = prompt("Enter your First name: ")
+// var last_name = prompt("Enter your Last name: ")
+
+// var Full_Name = (first_name +" "+ last_name)
+
+// alert("Hello! " + Full_Name + " Thanks you for coming to visit our webpage")
+
+//Task 02
+
+
+// var Phone_Model = prompt("What is your favorite mobile phone model?");
+
+// var length = Phone_Model.length;
+
+// if (Phone_Model !== null) {
+//     alert("The length of your input is: " + length);
+// } else {
+//     alert("No input provided.");
 // }
 
-// let result = capitalizedWords.join(" ");
-// console.log(result); 
-// var text = "hello i am saylani mass it training student"
-//  for (let i = 0; i < text.length; i++) {
-//     if (text.slice(i, i+7) ==="saylani") {
-//         console.log("saylani found")
-//         break;
-//     }
-    
-//  }
-//  var word = text.slice(11, 18 )
-// console.log(word)
+//Task 03
 
-// task 3
-// var text = "Hello I Am saylani Mass It Training Student"
-// var word = text.indexOf("saylani");
-// console.log(word)
-// for (var i = 0; i < text.length; i++){
-//     if(text.indexOf(i,i + 7) === "saylani"){
-//        break;
-//     }
+// var word = "Pakistani";
+
+// var letter = "n";
+
+// var index = word.indexOf(letter);
+
+// if (index !== -1) {
+//     alert("The index of letter " + letter + " in " + word + " is: " + index);
 // }
-// alert("Word Found")
 
-// task 4
 
-// var text = "hello i am saylani mass it training student"
-// var word = text.replace("saylani", "SMIT" )
-// console.log(word)
+//Task 04
+
+// var word = "Hello World";
+
+// var letter = "l";
+
+// var lastIndex = word.lastIndexOf(letter);
+
+// if (lastIndex !== -1) {
+//     alert("The last index of letter " + letter + " in " + word + " is: " + lastIndex);
+// }
+
+//Task 05
+
+// var word = "Pakistani";
+
+// var characterAtIndex = word.charAt(3);
+
+// alert("The character at index 3 in " + word + " is: " + characterAtIndex);
+
+//Task 06
+
+// var first_name = prompt("Enter your First name: ")
+// var last_name = prompt("Enter your Last name: ")
+
+// var Full_Name = first_name.concat(" ", last_name)
+
+// alert("Hello! " + Full_Name + " Thank you for coming to visit our webpage")
+
+//Task 07
+
+// var word = "Hyderabad";
+
+// console.log(word);
+
+// var replacedWord = word.replace("Hyder", "Islam");
+
+// console.log("The result after replacement is: " + replacedWord);
+
+//Task 08
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// var replacedLetters = message.replace(/and/g ,"&")
+
+// alert(replacedLetters);
+
+//Task 09
+
+// var str = "472";
+
+// var num = Number(str);
+
+// console.log("Original String: " + str);
+// console.log("Converted Number: " + num);
+
+//Task 10
+
+// var userInput = prompt("Please enter something:");
+
+// var upperCaseInput = userInput.toUpperCase();
+
+// alert(upperCaseInput);
